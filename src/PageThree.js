c1f6aaa3a1a7f387ce2d07b97bfa9c341bee8a5b
Divyam Mehta <divyammehta@Divyams-MacBook-Air.local>
2023-04-09 18:18:24 -0700

    minor updates

--- src/PageThree.js
+++ src/PageThree.js
@@ -10,46 +10,6 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme();
 
-
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     marginTop: theme.spacing(5),
-//     marginBottom: theme.spacing(5),
-//   },
-//   sliderContainer: {
-//     width: '80%',
-//     marginTop: theme.spacing(3),
-//     marginBottom: theme.spacing(3),
-//   },
-//   imageContainer: {
-//     display: 'flex',
-//     justifyContent: 'space-between',
-//     width: '80%',
-//     marginBottom: theme.spacing(3),
-//   },
-//   image: {
-//     width: '48%',
-//   },
-//   formContainer: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//     width: '80%',
-//   },
-//   textField: {
-//     marginBottom: theme.spacing(2),
-//   },
-//   button: {
-//     marginTop: theme.spacing(2),
-//     width: '50%',
-//   },
-// }));
-
 const marks = [
   {
     value: 0,
@@ -74,19 +34,23 @@ function PageThree() {
       <Typography variant="h4" align="center">
         Contact Us
       </Typography>
-      <div width='50%' marginBottom={theme.spacing(3)} marginTop={theme.spacing(3)}>
-        <Typography id="urgency-slider" gutterBottom>
-          Urgency
-        </Typography>
-        <Slider
-          value={urgency}
-          onChange={handleSliderChange}
-          aria-labelledby="urgency-slider"
-          marks={marks}
-          min={0}
-          max={100}
-        />
-      </div>
+      <Grid container spacing={2} justifyContent='center' alignItems='center' marginTop={theme.spacing(3)}>
+        <Grid item width='50%'>
+          <div width='80%' marginBottom={theme.spacing(3)} marginTop={theme.spacing(3)}>
+            <Typography id="urgency-slider" align='center' gutterBottom>
+              Urgency
+            </Typography>
+            <Slider
+              value={urgency}
+              onChange={handleSliderChange}
+              aria-labelledby="urgency-slider"
+              marks={marks}
+              min={0}
+              max={100}
+            />
+          </div>
+      </Grid>
+        </Grid>
       <div display='flex' justifyContent='space-between' width='50%' marginBottom={theme.spacing(3)}>
         <img src="http://www.easterngraphics.com/pcon/en/wp-content/uploads/2015/03/Musterwohnung-2.png" alt="apartment" width='25%' />
         <img src="https://youngandtheinvested.com/wp-content/uploads/invest-in-apartment-buildings.jpg" alt="building" width='25%' />
@@ -111,12 +75,12 @@ function PageThree() {
           variant="outlined"
           fullWidth
           multiline
-          rows={4}
+          rows={2}
           marginBottom= {theme.spacing(2)}
         />
         <Button marginTop={theme.spacing(2)}
           width= '50%'
-          variant="contained"
+          variant="outlined"
           color="primary"
           size="large"
         >
